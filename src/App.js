b83738d1eb40bfce7314ff30b0dbf29a14e0ac4d
adamrturman <adamrturman@gmail.com>
2021-01-28 14:15:07 -0600

    rendering data on the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [data, setData] = useState([])
+
+  const handleClick = (event)=> {
+    console.log("click")
+    fetch("https://ipfs.io/ipfs/QmbZiEejjAmdEmtF71WLPuY3dwkeMPCmcVxaj7N8aH56Zw/kava-4-export-20210122.json")
+    .then((response) => response.json())
+    .then((data)=> setData(data.validators))
+  }
+
+  console.log(data)
+  // console.log(typeof data)
+
+  const mappedData = data.map((transaction) => (
+    <li>{transaction.name}</li>
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Hello</button>
+      {mappedData}
     </div>
   );
 }
