0b7d907b1c4235671bd0403b8979a1bd5892c65c
adamrturman <adamrturman@gmail.com>
2021-01-28 18:06:56 -0600

    removed unnecessary files and refactored sum method to use forEach instead of loop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import MyChart from './Chart/Chart'
 
 
@@ -10,94 +9,81 @@ function App() {
 
 
 
-  const handleClick = (event)=> {
-    console.log("click")
+  const handleClick = (event) => {
     fetch("https://ipfs.io/ipfs/QmbZiEejjAmdEmtF71WLPuY3dwkeMPCmcVxaj7N8aH56Zw/kava-4-export-20210122.json")
-    .then((response) => response.json())
-    .then((data)=> setData(data.app_state.staking.delegations))
-    .then(setShowData(true))
+      .then((response) => response.json())
+      .then((data) => setData(data.app_state.staking.delegations))
+      .then(setShowData(true))
   }
 
-  console.log(data)
+  const totalTransactions = countTransactions(data)
+  const medianTransaction = median(data)
+  const largestTransaction = maxTransaction(data)
+  const maxInvestor = findInvestor(data, largestTransaction)
+  const sortedTransactions = sortedData(data)
+  const averageTransaction = sum(data) / countTransactions(data)
+  const addresses = data.map((transaction) => {
+    return transaction.delegator_address
+  })
+
+  const amounts = data.map((transaction) => {
+    return transaction.shares
+  })
+
+  const mappedSortedData = sortedTransactions.map((transaction) => {
+    return transaction
+  })
 
   function countTransactions(data) {
     return data.length
   }
 
   function median(data) {
-    return Math.floor(data.length/2)
+    return Math.floor(data.length / 2 + 1)
   }
 
- 
- const totalTransactions = countTransactions(data)
- const medianTransaction = median(data)
- const largestTransaction = maxTransaction(data)
- const maxInvestor = findInvestor(data, largestTransaction) 
- const sortedTransactions = sortedData(data)
- const sumTransactions = sum(data)
- const averageTransaction = sum(data) / countTransactions(data)
-
- function sortedData(data){
-   const allShares = data.map((transaction)=> {
-     return transaction.shares
-   })
-   return allShares.sort((a,b) => a-b)
- }
-
- function maxTransaction(data){
-  const allShares = data.map((transaction)=> {
-    return transaction.shares
-  })
-  return Math.max(...allShares)
-}
-
- function sum(data) {
-  const allAmounts = data.map((transaction)=> {
-    return transaction.shares
-  })
-  let total = 0
-  for (let i=0; i< allAmounts.length; i++) {
-    total += parseInt(allAmounts[i])
+  function sortedData(data) {
+    const allShares = data.map((transaction) => {
+      return transaction.shares
+    })
+    return allShares.sort((a, b) => a - b)
   }
-  return total
-}
 
-function findInvestor(data, amount) {
-  console.log("We are here")
-  for (let i=0; i< data.length; i++) {
-    if (data[i].shares == amount) {
-      return data[i].delegator_address
+  function maxTransaction(data) {
+    const allShares = data.map((transaction) => {
+      return transaction.shares
+    })
+    return Math.max(...allShares)
+  }
+
+  function sum(data) {
+    let total = 0
+    const allAmounts = data.forEach((transaction)=> {
+        total += parseInt(transaction.shares)
+    })
+    return total
+  }
+
+  function findInvestor(data, amount) {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].shares == amount) {
+        return data[i].delegator_address
+      }
     }
-  }  
-}
-
-const addresses = data.map((transaction) => {
-  return transaction.delegator_address
-})
-
-const amounts = data.map((transaction) => {
-  return transaction.shares
-})
-
-  const mappedSortedData = sortedTransactions.map((transaction)=> (
-    <div>{transaction}</div>
-  ))
- 
+  }
 
   return (
     <div className="App">
-      <button onClick={handleClick}>Hello</button>
+      <button onClick={handleClick}>Render Data</button>
       {showData ?
-      <div>
-        <div>There are {totalTransactions} total transactions</div>
-        <div>The median transaction is the index {medianTransaction}</div>
-        <div>The median value is {mappedSortedData[medianTransaction]}</div>
-        <div>The sum of all transactions is {sumTransactions}</div>
-        <div>The average of all transactions is {averageTransaction}</div>
-        <div>The maximum transaction is {largestTransaction} made by {maxInvestor}</div>
-        <MyChart addresses={addresses} amounts={amounts}/>
-      </div>
-      : null}
+        <div>
+          <div>There are {totalTransactions} total transactions</div>
+          <div>The median value is {mappedSortedData[medianTransaction]}</div>
+          <div>The average of all transactions is {averageTransaction}</div>
+          <div>The maximum transaction is {largestTransaction} made by {maxInvestor}</div>
+          <MyChart addresses={addresses} amounts={amounts} />
+        </div>
+        : null}
     </div>
   );
 }
--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -2,24 +2,22 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 
 
-
 export default function MyChart(props) {
-    console.log("props in chart", props)
     const data = {
         labels: [...props.addresses],
         datasets: [
-          {
-            label: "Shares per delegation",
-            data: [...props.amounts],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)"
-          },
+            {
+                label: "Shares per delegation",
+                data: [...props.amounts],
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
+            },
         ]
-      };
-  return (
-    <div className="App">
-      <Line data={data} />
-    </div>
-  );
+    };
+    return (
+        <div className="App">
+            <Line data={data} />
+        </div>
+    );
 }
