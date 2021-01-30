6a11aae5f7394d22726690c2e63e8f5c02a51b8d
adamrturman <adamrturman@gmail.com>
2021-01-29 19:10:33 -0600

    cleaned up css modules

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../App.module.css';
+import styles from './Chart.module.css';
 import { Line } from "react-chartjs-2";
 
 export default function MyChart(props) {
